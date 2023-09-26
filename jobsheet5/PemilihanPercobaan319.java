c3dc86eec8bb97933586760afa2c45e4bee74e8c
Muhammad Rafi Rajendra <rafi53417@gmail.com>
2023-09-26 21:03:38 +0700

    first commit

--- jobsheet5/PemilihanPercobaan319.java
+++ jobsheet5/PemilihanPercobaan319.java
@@ -18,7 +18,7 @@ public class PemilihanPercobaan319 {
             case '+':
                 hasil = angka1 + angka2;
                 System.out.println(angka1 + " + " + angka2 + "=" + hasil);
-                break;
+                
             case '-':
                 hasil = angka1 - angka2;
                 System.out.println(angka1 + " - " + angka2 + "=" + hasil);
@@ -31,6 +31,7 @@ public class PemilihanPercobaan319 {
                 hasil = angka1 / angka2;
                 System.out.println(angka1 + " / " + angka2 + "=" + hasil);
                 break;
+            default:
         }
 
 
