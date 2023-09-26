03f5b48749a6c9227a3b48ae9b4a29ca358d167d
Muhammad Rafi Rajendra <rafi53417@gmail.com>
2023-09-26 19:54:16 +0700

    first commit

--- jobsheet5/PemilihanPercobaan120.java
+++ jobsheet5/PemilihanPercobaan120.java
@@ -3,15 +3,11 @@ import java.util.Scanner;
 public class PemilihanPercobaan120 {
     public static void main(String[] args) {
         Scanner input20 = new Scanner(System.in);
-        System.out.println("Masukkan angka:  ");
+        System.out.print("Masukkan angka:  ");
         int angka = input20.nextInt();
-        if (angka % 2 == 0)
-            System.out.println("Angka "+angka+" bilangan genap");
-        else
-            System.out.println("Angka "+angka+" bilangan ganjil");
-
-        
-        
+        String status = (angka % 2 == 0) ? "genap" : "ganjil";
+        System.out.println("Angka " + angka + " bilangan " + status);
+          
     }
 
     
