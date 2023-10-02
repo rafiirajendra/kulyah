56de52ec634a87f36db0553d0bd50ea7bf58d765
Muhammad Rafi Rajendra <rafi53417@gmail.com>
2023-10-02 19:44:27 +0700

    first commit

---
+++ jobsheet/jobsheet6/Pemilihan2Percobaan119.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * Pemilihan2Percobaan119
+ */
+public class Pemilihan2Percobaan119 {
+
+    public static void main(String[] args) {
+        Scanner input19 = new Scanner(System.in);
+        
+        int tahun;
+
+        System.out.print("Masukkan Tahun: ");
+        tahun = input19.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0)
+                System.out.println("Tahun Kabisat");
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
+    }
+}
