13ad72c332c50d4e38dc0b2616fafb0f70d67bba
Muhammad Rafi Rajendra <rafi53417@gmail.com>
2023-10-04 06:52:20 +0700

    perubahan percobaan 1

--- jobsheet/jobsheet6/Pemilihan2Percobaan119.java
+++ jobsheet/jobsheet6/Pemilihan2Percobaan119.java
@@ -11,10 +11,10 @@ public class Pemilihan2Percobaan119 {
         tahun = input19.nextInt();
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0)
+            if ((tahun % 100) != 0){
                 System.out.println("Tahun Kabisat");
-        } else {
+        } else 
             System.out.println("Bukan Tahun Kabisat");
-        }
+        } 
     }
 }
