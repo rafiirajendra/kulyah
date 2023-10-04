cdce9e94bde4686694cf274bdf3614e5b292e2bc
Muhammad Rafi Rajendra <rafi53417@gmail.com>
2023-10-04 08:56:29 +0700

    perubahan equals percobaan 3

--- jobsheet/jobsheet6/Pemilihan3Percobaan319.java
+++ jobsheet/jobsheet6/Pemilihan3Percobaan319.java
@@ -13,7 +13,7 @@ public class Pemilihan3Percobaan319 {
         System.out.print("Masukkan Besarnya Penghasilan : ");
         penghasilan = input19.nextInt();
 
-        if (kategori.equalsIgnoreCase("pekerja")) {
+        if (kategori.equals("pekerja")) {
             if (penghasilan <= 2000000)
                 pajak = 0.1;
             else if (penghasilan <= 3000000)
@@ -22,7 +22,7 @@ public class Pemilihan3Percobaan319 {
                 pajak = 0.2;
             gajiBersih = (int) (penghasilan - (pajak * penghasilan));
             System.out.print("Penghasil Bersih : " + gajiBersih);
-        } else if (kategori.equalsIgnoreCase("pebisnis")){
+        } else if (kategori.equals("pebisnis")){
             if (penghasilan <= 2500000)
                 pajak = 0.15;
             else if (penghasilan <= 3500000)
