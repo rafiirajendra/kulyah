19cd9868f60a22f892e0249002e9f88fa84eca25
Muhammad Rafi Rajendra <rafi53417@gmail.com>
2023-10-24 22:25:50 +0700

    perubahan pada percobaan 4

--- jobsheet/jobsheet9/BubbleSortExample.java
+++ jobsheet/jobsheet9/BubbleSortExample.java
@@ -3,12 +3,12 @@ package jobsheet9;
 public class BubbleSortExample {
     public static void main(String[] args) {
         
-        int[] intData = {34, 18, 72, 32, 54, 43};
+        int[] intData = {34, 18, 87, 72, 32, 54, 43};
         int temp = 0;
 
         for (int i=0; i < intData.length; i++){
             for (int j=1; j < intData.length-i; j++){
-                if (intData[j-1] > intData[j]) {
+                if (intData[j-1] < intData[j]) {
                         //swap elemen
                         temp = intData[j];
                         intData[j] = intData[j-1];
