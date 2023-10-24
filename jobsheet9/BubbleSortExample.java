b8298cbcfb232a977739aeba32860ea6452b450d
Muhammad Rafi Rajendra <rafi53417@gmail.com>
2023-10-24 19:55:08 +0700

    jobsheet 9

---
+++ jobsheet9/BubbleSortExample.java
@@ -0,0 +1,25 @@
+package jobsheet9;
+
+public class BubbleSortExample {
+    public static void main(String[] args) {
+        
+        int[] intData = {34, 18, 72, 32, 54, 43};
+        int temp = 0;
+
+        for (int i=0; i < intData.length; i++){
+            for (int j=1; j < intData.length-i; j++){
+                if (intData[j-1] > intData[j]) {
+                        //swap elemen
+                        temp = intData[j];
+                        intData[j] = intData[j-1];
+                        intData[j-1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Hasil pengurutan:");
+        for (int i = 0; i < intData.length; i++){
+            System.out.println(intData[i]);
+        }
+    }
+}
